55b9d742538657038fd4cfea468771a1ea40aa88
Uchchash346 <mru416@gmail.com>
2022-10-19 15:06:27 +0600

    admin dashboard completed

---
+++ src/nn.js
@@ -0,0 +1,18 @@
+// {
+//     "langName": "Chinese",
+//     "langId": "zh",
+//     "icon": "cn",
+//     "lang": "Ch"
+// },
+// {
+//     "langName": "French",
+//     "langId": "fr",
+//     "icon": "fr",
+//     "lang": "Fr"
+// },
+// {
+//     "langName": "Janpanese",
+//     "langId": "ja",
+//     "icon": "jp",
+//     "lang": "Jp"
+// }
